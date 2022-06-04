e05a94cb86f3a52d9b9b629f566be8e6097cee67
Alexander Snitko <asyncamisu@gmail.com>
2022-06-04 22:07:58 +0300

    welcome view added

---
+++ FlashCard/Welcome/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  FlashCard
+//
+//  Created by Alexander Snitko on 4.06.22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
