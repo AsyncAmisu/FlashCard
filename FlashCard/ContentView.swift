e5a26ba21972da64c905a733bab6ade125501455
Alexander Snitko <asyncamisu@gmail.com>
2022-06-04 19:59:58 +0300

    Initial Commit

---
+++ FlashCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashCard
+//
+//  Created by Alexander Snitko on 4.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Alexander Snitko on 4.06.22.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
