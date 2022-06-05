b8cbcf1979e67ab67c7972b58f6a034d6f4b88e0
Alexander Snitko <asyncamisu@gmail.com>
2022-06-05 23:18:56 +0300

    text and image

--- FlashCard/Welcome/WelcomeView.swift
+++ FlashCard/Welcome/WelcomeView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("welcome", bundle: nil)
+                .resizable()
+                .aspectRatio(1 / 1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .saturation(0.5)
+                .blur(radius: 5)
+                .opacity(0.08)
+            HStack {
+                Image(systemName: "table")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 3))
+                    .background(Color(white: 0.9))
+                    .clipShape(Circle())
+                .foregroundColor(Color.red)
+                Text("Welcome to FlashCard")
+                    .font(.system(size: 30))
+                    .bold()
+                    .foregroundColor(.red)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+            }
+        }
     }
 }
 
