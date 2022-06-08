49663fd79f4cb5cab5216bb4e0607a66fec9f404
Alexander Snitko <asyncamisu@gmail.com>
2022-06-08 13:54:13 +0300

    register view textfield

--- FlashCard/Welcome/RegisterView.swift
+++ FlashCard/Welcome/RegisterView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var name: String = ""
+    
     var body: some View {
-        ZStack {
-            WelcomeBackgroundImage()
-            VStack {
-                WelcomeMessageView()
-            }
+        VStack {
+            Spacer()
+            WelcomeMessageView()
+            TextField("Enter your name...", text: $name)
+                .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+                .background(Color.white)
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(Color.blue))
+                .shadow(color: Color.gray.opacity(0.4), radius: 3, x: 1, y: 2)
+            Spacer()
         }
+        .background(WelcomeBackgroundImage())
+        .padding()
     }
 }
 
